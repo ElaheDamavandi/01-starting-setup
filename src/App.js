3f384cbcec67539313a02189e77e9d537bcde34d
Sura Dyar <suradyarfromnowon@gmail.com>
2022-08-22 21:45:53 -0700

    All component compeleted 03

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expense from "./components/Expense";
 
 function App() {
+  const expenses = [
+    {
+      title: "car01",
+      amount: 245,
+      date: new Date(2021, 2, 23),
+    },
+    {
+      title: "car02",
+      amount: 497,
+      date: new Date(2020, 2, 28),
+    },
+    {
+      title: "car03",
+      amount: 300,
+      date: new Date(2021, 6, 28),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expense items={expenses}/>
     </div>
   );
 }
